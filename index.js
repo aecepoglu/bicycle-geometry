ed4e7378969f790d43c609a9e69bbb49c15936dd
aecepoglu <aecepoglu@fastmail.fm>
2018-11-08 23:31:51 +0300

    switched to Crocks

--- index.js
+++ index.js
@@ -9,37 +9,25 @@ import {
 	map,
 	path,
 	prop,
-        sequence,
         toUpper,
-} from "ramda";
+} from "ramda"
 
-import IO from "./types/io";
-import {Just, Nothing} from "sanctuary-maybe";
-import {Left, Right} from "sanctuary-either";
-import Emitter from "./types/emitter";
-const {bimap, maybeToEither} = require("./util").factory({Right, Left});
+import IO from "crocks/IO"
+import Maybe from "crocks/Maybe"
+import Emitter from "./types/emitter"
 
-const {
-	maybeToEither
-}  = require("./utils").factory({
-	Just, Nothing, Right, Left
-});
-
-const {
-	maybeToEither
-}  = require("./utils").factory({
-	Just, Nothing, Right, Left
-});
+import maybeToEither from "crocks/Either/maybeToEither"
+import either from "crocks/pointfree/either"
 
 
 // point :: (Number, Number) -> Point
 const point = (x, y) => ({
 	x: x,
 	y: y
-});
+})
 
 // radians :: Number -> Angle
-const radians = divide(2 * Math.PI);
+const radians = divide(2 * Math.PI)
 
 // tube :: (String, Float, Point, Angle) -> 
 const tube = (name, length, P, angle) => ({
@@ -47,36 +35,36 @@ const tube = (name, length, P, angle) => ({
 	length: length,
 	P: P,
 	angle: angle
-});
+})
 
 // print :: String -> a -> a
 const print = curry((prefix, x) => {
-	console.log(prefix, x);
-	return x;
-});
+	console.log(prefix, x)
+	return x
+})
 
 // pprintIO :: String -> a -> IO a
-const pprintIO = map(IO.of, print);
+const pprintIO = map(IO.of, print)
 
 // pprintIO :: a -> IO a
-const printIO = pprintIO("PRINT");
+const printIO = pprintIO("PRINT")
 
 // $ :: String -> IO DOM
-const $ = sel => IO.of(document.querySelector(sel));
+const $ = sel => IO.of(document.querySelector(sel))
 
 // setHtml :: String -> String -> IO Node
 const setHtml = curry((sel, html) => {
 	return map(
 		x => {
-			x.innerHTML = html;
-			return x;
+			x.innerHTML = html
+			return x
 		},
 		$(sel)
-	);
-});
+	)
+})
 
 // parseFloatSafe :: String -> Maybe Float
-const parseFloatSafe = map(x => Number.isNaN(x) ? Nothing : Just(x), parseFloat);
+const parseFloatSafe = map(x => Number.isNaN(x) ? Maybe.Nothing() : Maybe.Just(x), parseFloat)
 
 // listevEvents :: IO Textbox -> Emitter ChangeEvent
 const listenEvents = (io, ev) => compose(
@@ -84,30 +72,29 @@ const listenEvents = (io, ev) => compose(
 	map(Emitter.fromResultOf),
 	// IO f(cb)
 	chain(pprintIO("3")),
-	map(f => curryN(2, f)(ev)), //TODO curry this too
+	map(f => curryN(2, f)(ev)), //TODO curry this too. 'flip' may help
 	// IO f(ev, cb)
 	chain(pprintIO("2")),
 	map(prop("addEventListener")),
 	// IO Textbox
 	chain(pprintIO("1")),
 	$,
-)(io, ev);
+)(io, ev)
 
-console.log(setHtml("#forkLenOut")("hello"));
-listenEvents("#forkLen", "change")
-	.performUnsafeIO()
+console.log(setHtml("#forkLenOut")("hello"))
+listenEvents("#forkIn > input", "change")
+	.run()
 	.subscribe(e => {
 		compose(
-                        prop("value"), //FIXME don't extract the value like this.
-                        bimap(
-                                compose(
-                                        setHtml("#forkLenOut"),
-                                        toUpper,
-                                ),
-                                setHtml("#forkLenOut"),
-                        ),
+			either(
+				compose(
+					setHtml("#forkIn > .err"),
+					toUpper
+				),
+				setHtml("#forkLenOut") //TODO also clear out the error
+			),
                         maybeToEither("not a number"),
 			parseFloatSafe,
 			path(["target", "value"]),
-		)(e).performUnsafeIO();
-	});
+		)(e).run()
+	})
