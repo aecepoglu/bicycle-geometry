db0514f42a70f61cef5a133a7d4e79c61eb7dae0
Ahmet Emre <ahmet.cepoglu@esensi.com.tr>
2018-11-12 19:58:53 +0300

    able to show full bicycle frame

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 const path = require("path");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
 	devtool: "eval-source-map",
 	mode: process.env["WEBPACK_MODE"] || "development",
 
-	entry: "./index.js",
+	entry: "./src/index.js",
 	output: {
 		path: path.resolve("./public"),
-		filename: "code.js"
+		filename: "code.js",
 	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				use: [
+					{ loader: "eslint-loader" },
+				],
+			}
+		]
+	},
+
+	plugins: [
+		new WebpackNotifierPlugin({contentImage: undefined}),
+	],
 
 	devServer: {
 		contentBase: path.resolve("./public"),
-		port: 3000
+		port: 3000,
 	}
 };
